27f1c0d7b43f658f4bd16b4d05c0beefa1fe6385
qj0r9j0vc2 <jinwoo794533@gmail.com>
2024-02-26 19:49:21 +0900

    Remove provider before

--- api/v1alpha1/zz_generated.deepcopy.go
+++ api/v1alpha1/zz_generated.deepcopy.go
@@ -3,7 +3,6 @@
 
 /*
 Copyright 2024 B-Harvest Corporation.
-Copyright 2022 Strangelove Ventures LLC.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
